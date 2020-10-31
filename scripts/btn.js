559e6c80d83ce233720570249c754f47f3051804
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-31 11:15:29 -0300

    Comments

--- scripts/btn.js
+++ scripts/btn.js
@@ -1,13 +1,16 @@
 
 // ------------- Sotial midia button behavior --------------------
 
+
+//-----Instagram's button behavior and effects 
+
 function changeImg() {
 	var button = document.getElementById('instaImg3').style.display;
 
 	if (button == 'none') {		
 
-		//-----Instagram's button behavior and effects 
-		//-----Active
+		
+		//-----Instagram when "Active" :
 		
 		//Button's interface behavior
 
@@ -36,34 +39,53 @@ function changeImg() {
 		document.getElementById('instaImg2').style.display = 'block';
 		document.getElementById('instaImg3').style.display = 'none';
 
+		//Preview post are behavior
+
 		document.getElementById('post-preview').style.display = 'none';
 		document.getElementById('waiting').style.display = 'block';
 	}
 }
+
+
+		//-----Linkedin's button behavior and effects 
+
 
 function changeImg_1() {
 	var button = document.getElementById('inImg3').style.display;
 
 	if (button == 'none') {
 
+		//-----Linkedin when "Active" :
+
         //Linkedin button behavior and effects 
 
 		document.getElementById('inImg1').style.display = 'none';
 		document.getElementById('inImg2').style.display = 'none';
-        document.getElementById('inImg3').style.display = 'block';
+		document.getElementById('inImg3').style.display = 'block';
+		
+		//Preview's interface behavior
 		
 		document.getElementById('post-preview-in').style.display = 'block';		
         document.getElementById('post-preview').style.display = 'none';
 		document.getElementById('waiting').style.display = 'none';
 
+		//Instagram's button behavior
+
 		document.getElementById('instaImg1').style.display = 'block';
 		document.getElementById('instaImg2').style.display = 'block';
 		document.getElementById('instaImg3').style.display = 'none';
 	} else {
+
+		//Unpressing
+
+		//Button's interface behavior
+
 		document.getElementById('inImg1').style.display = 'block';
 		document.getElementById('inImg2').style.display = 'block';
 		document.getElementById('inImg3').style.display = 'none';
 
+		//Preview post are behavior
+		
 		document.getElementById('post-preview-in').style.display = 'none';		
         document.getElementById('post-preview').style.display = 'none';
 		document.getElementById('waiting').style.display = 'block';
